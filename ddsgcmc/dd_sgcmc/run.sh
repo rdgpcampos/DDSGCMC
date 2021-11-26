484ae44bec78aa166c154c8c8f4596572b7647c6
Rodrigo Campos <70313745+rdgpcampos@users.noreply.github.com>
2021-11-26 11:45:38 +0900

    Update run.sh

--- ddsgcmc/dd_sgcmc/run.sh
+++ ddsgcmc/dd_sgcmc/run.sh
@@ -10,5 +10,5 @@ RND_SEED=5476378
 LMP_PATH=$HOME/lammps-stable_12Dec2018/lammps-stable_12Dec2018/src
 export LD_LIBRARY_PATH=${LMP_PATH}:${LD_LIBRARY_PATH}
 
-mpirun -np 8  $BIN $N $INP $MCT 1>stdout 2>stderr
+mpirun -np 8  $BIN $N $INP $MCT $RND_SEED 1>stdout 2>stderr
 
